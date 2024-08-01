9227b7789ed6a81ae4652ef867d06149d68540cf
CSdere <callum_connell2005@outlook.com>
2024-08-01 22:30:17 +0100

    Update AllPosts.tsx

--- components/AllPosts.tsx
+++ components/AllPosts.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from 'react';
 import { db } from '@/firebase'; // Adjust the import path as necessary
 import { collection, getDocs } from 'firebase/firestore';
