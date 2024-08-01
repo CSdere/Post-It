02ae8076b9eec88a9e11754b334b8849c57e3d48
CSdere <callum_connell2005@outlook.com>
2024-08-01 22:24:12 +0100

    Update AllPosts.tsx

--- components/AllPosts.tsx
+++ components/AllPosts.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import React, { useEffect, useState } from 'react';
 import { db } from '@/firebase'; // Adjust the import path as necessary
 import { collection, getDocs, DocumentData } from 'firebase/firestore';
@@ -20,10 +18,10 @@ const AllPosts: React.FC = () => {
     const fetchPosts = async () => {
       try {
         const querySnapshot = await getDocs(collection(db, 'posts'));
-        const postsData = querySnapshot.docs.map((doc): Post => ({
-          id: doc.id,
-          ...doc.data() as Post
-        }));
+        const postsData = querySnapshot.docs.map((doc): Post => {
+          const data = doc.data() as Omit<Post, 'id'>;
+          return { id: doc.id, ...data };
+        });
 
         // Sort posts by createdAt field in descending order
         const sortedPosts = postsData.sort((a, b) => b.createdAt.seconds - a.createdAt.seconds);
@@ -37,7 +35,7 @@ const AllPosts: React.FC = () => {
   }, []);
 
   return (
-    <div className="lg:w-1/3 p-6 flex flex-col items-center allPosts">
+    <div className="flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-6">Posts</h1>
       {posts.map(post => (
         <div key={post.id} className="w-2/3 mb-4 p-4 border rounded-lg shadow-lg">
